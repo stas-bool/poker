d9ac81f9080bf5958b143fa76156c0ebe7b34748
Stanislav <Stas-vps>
2019-01-03 13:52:55 +0700

    Добавил аналитику

--- index.php
+++ index.php
@@ -18,6 +18,30 @@
 	<script type="text/javascript" src="js/scripts.js"></script>
 
     <title>Poker Combinations</title>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131556579-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-131556579-1');
+</script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(51780191, "init", {
+id:51780191,
+	clickmap:true,
+	trackLinks:true,
+	accurateTrackBounce:true
+	});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/51780191" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </head>
 <body>
 	<div class="container">
